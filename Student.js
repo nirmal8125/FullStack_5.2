4bab150b5750c1a4c5647566ba46ac1858e4c5e4
nirmal8125 <152059026+nirmal8125@users.noreply.github.com>
2025-10-05 22:25:03 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    course: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+});
+
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
